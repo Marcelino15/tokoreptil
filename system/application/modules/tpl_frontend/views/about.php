f05b38c69b41903c5b8deaedc51fdf399ce22fff
Marcelino15 <tegar.marcelino@gmail.com>
2019-06-21 17:33:49 +0700

    21-6-19

--- system/application/modules/tpl_frontend/views/about.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$this->load->view('01_head');
-$this->load->view('02_header');
-$this->load->view('12_about');
-$this->load->view('10_footer');
-$this->load->view('11_javascript');
-?>
--- system/application/modules/tpl_frontend/views/beranda.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$this->load->view('03_slide');
-$this->load->view('04_banner');
-$this->load->view('05_newproduct');
-$this->load->view('06_banner2');
-$this->load->view('07_blog');
-$this->load->view('09_shipping');
-?>
